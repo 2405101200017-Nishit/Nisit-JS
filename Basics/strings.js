2475dc27883a978f8b5a13ddffe9b6c8af426d73
NISHIT <nisht@NISHTs-MacBook.local>
2026-07-20 21:42:34 +0530

    Strings in JS

---
+++ Basics/strings.js
@@ -0,0 +1,32 @@
+const name = "Nisit"
+const repoCount = 4
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("Nishit-NK-com")
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('s'));
+
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8, 4)
+// console.log(anotherString);
+
+// const newStringOne = "   Nishit   "
+// console.log(newStringOne)
+// console.log(newStringOne.trim())
+
+const url = "https://nishit.com/nishit%20kakkad"
+
+console.log(url.replace('%20', '-'))
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
