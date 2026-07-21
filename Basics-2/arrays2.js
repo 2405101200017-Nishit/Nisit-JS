707fed6e69664d039870fa8db19764e7a37de003
NISHIT <nisht@NISHTs-MacBook.local>
2026-07-21 19:10:42 +0530

    Arrays 2

---
+++ Basics-2/arrays2.js
@@ -0,0 +1,34 @@
+const Marvel = ["Thor", "Hulk", "Ironman"]
+
+const DC = ["Superman", "Flash", "Batman"]
+
+// Marvel.push(DC)
+
+// console.log(Marvel);
+
+// const allHeroes = Marvel.concat(DC)
+// console.log(allHeroes);
+
+const allNewHeroes = [...Marvel, ...DC]
+
+// console.log(allNewHeroes);
+
+const anotherArray = [1, 2, 3, [4, 5, 6],7 , [6, 7, [4, 5]]]
+
+const realAnotherArray = anotherArray.flat(Infinity)
+
+console.log(realAnotherArray);
+
+
+console.log(Array.isArray("Nishit"));
+console.log(Array.from("Nishit"));
+
+console.log(Array.from({name: "Nishit"})); // Interesting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
+
+ 
