1667a7d2679c62742ee0db696850c543992abc8e
NISHIT <nisht@NISHTs-MacBook.local>
2026-07-22 17:30:53 +0530

    Functions done

--- Basics-3/functions.js
+++ Basics-3/functions.js
@@ -35,4 +35,42 @@ function loginUserMessage(username = "Sam"){
 }
 
 // console.log(loginUserMessage("Nishit"))
-console.log(loginUserMessage())
+//console.log(loginUserMessage())
+
+function calculateCarPrice (...num1) {
+return num1
+}
+
+// console.log(calculateCarPrice(200, 400, 500))
+
+function calculateBatPrice (val1, val2, ...num1) {
+return num1
+}
+
+// console.log(calculateBatPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "Nishit",
+    price: 199
+}
+
+function handleObject(anyobject) {
+    //console.log(`Username is ${anyobject.username} and price is ${anyobject.price}.`);
+    
+}
+//handleObject(user)
+
+handleObject ({
+    username: "Sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+//console.log(returnSecondValue(myNewArray));
+//console.log(returnSecondValue([200, 900, 500, 1000]));
+
