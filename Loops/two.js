2631e338197519b5cf2925bc6ce055c84baf966c
NISHIT <nisht@NISHTs-MacBook.local>
2026-07-25 17:50:08 +0530

    Loops two

---
+++ Loops/two.js
@@ -0,0 +1,28 @@
+// While 
+
+let i = 0
+
+while (i <= 10) {
+   // console.log(`value of index is ${i}`);
+    i = i + 2
+    
+}
+
+let arrayOne = ["Spiderman", "Ironman", "Hulk"] 
+
+let arr = 0
+while ( arr < arrayOne.length ) {
+    //console.log(`Value is. ${arrayOne[arr]}`);
+    arr = arr + 1
+    
+}
+
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score ++
+    
+
+} while(score <= 10)
