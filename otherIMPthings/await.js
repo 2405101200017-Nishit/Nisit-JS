67e816125d95c79a877eb0be312709623d763941
NISHIT <nisht@NISHTs-MacBook.local>
2026-08-10 18:00:49 +0530

    practice

--- otherIMPthings/await.js
+++ otherIMPthings/await.js
@@ -1,24 +1,47 @@
-const studentPromise = new Promise((resolve, reject) => {
+// const studentPromise = new Promise((resolve, reject) => {
 
-    setTimeout(() => {
-        resolve({
-            name: "Nisit",
-            age: 20
-        });
-    }, 2000);
+//     setTimeout(() => {
+//         resolve({
+//             name: "Nisit",
+//             age: 20
+//         });
+//     }, 2000);
 
-});
+// });
 
-async function getStudent() {
+// async function getStudent() {
 
+//     try {
+//         const student = await studentPromise;
+
+//         console.log(student);
+
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+// getStudent();
+
+
+
+const Log = new Promise((resolve,reject) => {
+    setTimeout(function(){
+        resolve()
+        console.log("Hello Nishit")
+    },1000)
+    
+})
+
+async function getLog(){
     try {
-        const student = await studentPromise;
-
-        console.log(student);
-
-    } catch (error) {
-        console.log(error);
+        const greeting = await Log
+        console.log(greeting);
+        
     }
+    catch (error){
+        console.log(error);
+        
+    }
+    
 }
-
-getStudent();
