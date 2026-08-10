289e2f740c75c6e34bd5e5c963cf11ffce2a64f3
NISHIT <nisht@NISHTs-MacBook.local>
2026-08-10 17:31:34 +0530

    Before React

--- BeforeReact/fetch.js
+++ BeforeReact/fetch.js
@@ -6,3 +6,15 @@ fetch("https://jsonplaceholder.typicode.com/users")
     .catch(error => {
         console.log(error);
     });
+
+    // async await
+
+    async function getUsers() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+
+    const data = await response.json();
+
+    console.log(data);
+}
+
+getUsers();
